78ab18e2b03cf41f8064f90cc0131c4193b6b1d0
emiliano080591 <emilianoruben91@gmail.com>
2022-05-13 15:25:27 -0500

    ADD fain fan out cancel go routines

--- concurrency_go/patterns/fanIn_fanOut/fan.go
+++ concurrency_go/patterns/fanIn_fanOut/fan.go
@@ -2,50 +2,67 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 	"time"
 )
 
-// generator() -> square() -> print
-func generator(nums ...int) <-chan int {
+// generator() -> square() ->
+//								merge -> print
+// 			   -> square() ->
+func generator(done <-chan struct{}, nums ...int) <-chan int {
 	out := make(chan int)
 	go func() {
+		defer close(out)
 		for _, n := range nums {
-			fmt.Println("Proccessing in generator num->", n)
-			time.Sleep(time.Second)
-			out <- n
+			select {
+			case out <- n:
+				fmt.Println("Proccessing in generator num->", n)
+				time.Sleep(time.Second)
+			case <-done:
+				return
+			}
+
 		}
-		close(out)
 	}()
 
 	return out
 }
 
-func square(in <-chan int) <-chan int {
+func square(done <-chan struct{}, in <-chan int) <-chan int {
 	out := make(chan int)
 
 	go func() {
+		defer close(out)
 		for n := range in {
-			fmt.Println("Proccessing in square num->", n)
-			time.Sleep(time.Second)
-			out <- n * n
+			select {
+			case out <- n * n:
+				fmt.Println("Proccessing in square num->", n)
+				time.Sleep(time.Second)
+			case <-done:
+				return
+			}
 		}
-		close(out)
 	}()
 	return out
 }
 
-func merge(cs ...<-chan int) <-chan int {
+func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
 	// merge a list of channels to a single channel
 	out := make(chan int)
 	var wg sync.WaitGroup
 
 	// sincroniza
 	output := func(c <-chan int) {
+		defer wg.Done()
 		for n := range c {
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
 			out <- n
 		}
-		wg.Done()
 	}
 	wg.Add(len(cs))
 
@@ -61,12 +78,19 @@ func merge(cs ...<-chan int) <-chan int {
 }
 
 func main() {
-	in := generator(2, 3, 4, 5, 6, 7)
+	done := make(chan struct{})
+	in := generator(done, 2, 3, 4, 5, 6, 7)
 
-	ch1 := square(in)
-	ch2 := square(in)
+	ch1 := square(done, in)
+	ch2 := square(done, in)
 
-	for n := range merge(ch1, ch2) {
+	/*for n := range merge(ch1, ch2) {
 		fmt.Println(n)
-	}
+	}*/
+
+	out := merge(done, ch1, ch2)
+	fmt.Println(<-out)
+	close(done)
+	g := runtime.NumGoroutine()
+	fmt.Println("Num goroutines->", g)
 }
