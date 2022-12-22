98e060ce5bc103fe83ee441b63135e5ba4b2de7e
Emiliano Martinez <emiliano.martinez@mercadolibre.com.mx>
2022-12-22 15:44:36 -0600

    Finish the project

--- project_email/cmd/web/handlers.go
+++ project_email/cmd/web/handlers.go
@@ -156,7 +156,11 @@ func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 	// get the id of the plan that is chosen
 	id := r.URL.Query().Get("id")
 
-	planID, _ := strconv.Atoi(id)
+	planID, err := strconv.Atoi(id)
+	if err != nil {
+		app.ErrorLog.Println("Error getting plan ID:", err)
+	}
+
 	// get the plan from the database
 	plan, err := app.Models.Plan.GetOne(planID)
 	if err != nil {
@@ -213,12 +217,26 @@ func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 		// test app error chan
 		app.ErrorChan <- errors.New("come custom error")
 	}()
+	// subscribe the user to a plan
+	err = app.Models.Plan.SubscribeUserToPlan(user, *plan)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Error subscribing to plan!")
+		http.Redirect(w, r, "/members/plan", http.StatusSeeOther)
+		return
+	}
+	u, err := app.Models.User.GetOne(user.ID)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Error getting user from database!")
+		http.Redirect(w, r, "/members/plan", http.StatusSeeOther)
+		return
+	}
 
-	// send an email with the manual attached
-
-	// subscribe the user to an account
+	app.Session.Put(r.Context(), "user", u)
 
 	// redirect
+	app.Session.Put(r.Context(), "flash", "Subscribed!")
+	http.Redirect(w, r, "/members/plans", http.StatusSeeOther)
+	return
 }
 
 func (app *Config) generateManual(u data.User, plan *data.Plan) *gofpdf.Fpdf {
@@ -240,12 +258,13 @@ func (app *Config) generateManual(u data.User, plan *data.Plan) *gofpdf.Fpdf {
 	pdf.SetFont("Arial", "", 12)
 	pdf.MultiCell(0, 4, fmt.Sprintf("%s %s", u.FirstName, u.LastName), "", "C", false)
 	pdf.Ln(5)
-	pdf.MultiCell(0, 4, fmt.Sprintf("%s User Guide"), "", "C", false)
+	pdf.MultiCell(0, 4, fmt.Sprintf("%s User Guide", plan.PlanName), "", "C", false)
 
 	return pdf
 }
 
 func (app *Config) getInvoice(u data.User, plan *data.Plan) (string, error) {
+	app.InfoLog.Println("amount is ", plan.PlanAmountFormatted)
 	return plan.PlanAmountFormatted, nil
 }
 
